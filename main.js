fdca397cdeb9bc1006a286d12f68521ca1393ae9
thalitasilva007 <thalitasilva007@gmail.com>
2025-03-31 17:32:45 +0000

    função para calcular a gorgeta e o total por pessoa finalizada

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ let buttonSelected = null
 
 function receiveBillValue(){
     bill = document.querySelector("#bill").valueAsNumber
+    calculate()
 }
 
 function receiveNumberOfPeopleValue() {
     numberOfPeople = document.querySelector("#people").valueAsNumber
+    calculate()
 }
 
 function receiveTipPercentageValue(value){
@@ -26,6 +28,7 @@ function receiveTipPercentageValue(value){
 
    buttonSelected = document.querySelector(`#button-${value}`)
    buttonSelected.classList.add("button-selected")
+   calculate()
 }
 
 function receiveCustomTipPercentageValue(){
@@ -35,5 +38,21 @@ function receiveCustomTipPercentageValue(){
     }
 
     tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+    calculate()
 }
 
+function calculate(){
+    if(bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
+        let amountStrong = document.querySelector(".amount strong")
+        let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+        amountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+        let totalStrong = document.querySelector(".total strong") 
+        let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+        totalStrong.innerText = `$${totalPerson.toFixed(2)}`
+    }
+
+   else{
+       console.log("preencha tudo")
+   }
+}
